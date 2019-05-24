fd262c583ea901ee36372336a00e17d4014e1b3d
Heather Smith <heathersmith@google.com>
2019-05-24 20:42:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var myArr = []
+  for (var i = 0; i < musicians.length; i++) {
+    myArr.push(musicians[i]+ ' plays '+instruments[i])
+  }
+  return myArr
+}
+
+function johnLennonFacts(jlFacts) {
+  var i = 0
+  var myArr = []
+  while (i < jlFacts.length) {
+    myArr.push(jlFacts[i]+'!!!')
+    i++
+  }
+  return myArr
+}
+
+iLoveTheBeatles(num) {
+  var myArry = []
+  
+}
